c72b3e4320934158039a5182a8882e5bba569baf
enismusliu <91558330+enismusliu@users.noreply.github.com>
2022-01-19 13:00:53 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,9 @@ import BlogList from "./BlogList";
 
 const Home = () => {
      const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    { title: 'My new website', body: 'lorem ipsum...', author: 'Enis1', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Enis2', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Enis3', id: 3 }
      ])
     return (
         <div className="home">
